6771f1995f3a60914bebdf341dc4cd0003588b18
Joao Stafin <stafin@gmail.com>
2017-05-12 11:20:34 -0300

    adicionado linha no master

--- autenticacao/aut1.php
+++ autenticacao/aut1.php
@@ -1,3 +1,4 @@
 aut 1
 aut index
 alterado junto com o index
+adicionado linha no master
--- index.php
+++ index.php
@@ -2,3 +2,4 @@ index 1
 autenticado
 alteracao depois do autenticado
 alterado index dentro da autenticacao
+adicionado linha no master
